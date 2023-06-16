2ecb355ea3274996f17e4005558b085ea9f2c7fe
andreylavrusenko <m>
2023-06-16 22:54:12 +0300

    get all workouts, create workout

---
+++ controllers/workoutFunctionControllers.js
@@ -0,0 +1,73 @@
+const createError = require("../error.js");
+const {pool} = require("../db.js");
+const jwt = require("jsonwebtoken");
+const {v4: uuidv4} = require("uuid");
+
+const createNewWorkout = (req, res, next) => {
+    try {
+
+        const authToken = req.headers.token;
+
+        if (authToken) {
+
+            const decoded = jwt.verify(authToken, process.env.SECRET_JWT)
+
+            const workout_id = uuidv4()
+
+            //TODO Возможно нудно будет сдлеать JSON.parse
+
+            const sqlCreateWorkout = "INSERT INTO workout (id, trainer_id) VALUES (?, ?)"
+            const dataCreateWorkout = [workout_id, decoded.id]
+
+            pool.query(sqlCreateWorkout, dataCreateWorkout, async (error, result) => {
+                if (error) return res.status(400).json({message: error, resultCode: 1})
+
+                return res.status(201).json({
+                    resultCode: 0,
+                    workout_id
+                })
+
+            })
+        }
+
+
+    } catch (err) {
+        next(createError(400, 'Что-то пошло не так!'))
+    }
+}
+
+
+const getAllUserWorkouts = (req, res, next) => {
+    try {
+
+        const authToken = req.headers.token;
+
+        if (authToken) {
+
+            const decoded = jwt.verify(authToken, process.env.SECRET_JWT)
+
+            const sqlGetAllWorkout = "SELECT * FROM workout WHERE trainer_id = ?"
+            const data = [decoded.id]
+
+
+            pool.query(sqlGetAllWorkout, data, async (error, result) => {
+                if (error) return res.status(400).json({message: error, resultCode: 1})
+
+                return res.status(200).json({
+                    resultCode: 0,
+                    result
+                })
+
+            })
+        }
+
+        } catch (err) {
+        next(createError(400, 'Что-то пошло не так!'))
+    }
+}
+
+
+module.exports = {
+    createNewWorkout,
+    getAllUserWorkouts,
+}
